966a50c69271dd764078b99f341af82db2a3cf0e
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2020-05-06 14:21:34 -0600

    Initial commit

---
+++ functions.php
@@ -0,0 +1,83 @@
+<?php
+
+function load_stylesheet()
+{
+    wp_register_style('animate', get_template_directory_uri() . '/css/vendor/animate.min.css', '', '3.7.2', 'all');
+    wp_enqueue_style('animate');
+
+    wp_register_style('aos', get_template_directory_uri() . '/css/vendor/aos.min.css', '', false, 'all');
+    wp_enqueue_style('aos');
+
+    wp_register_style('hover', get_template_directory_uri() . '/css/vendor/hover.min.css', '', '2.3.2', 'all');
+    wp_enqueue_style('hover');
+
+    wp_register_style('fontawesome', get_template_directory_uri() . '/css/vendor/fontawesome/css/all.min.css', '', '5.12.1', 'all');
+    wp_enqueue_style('fontawesome');
+
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/vendor/bootstrap.min.css', '', '4.4.1', 'all');
+    wp_enqueue_style('bootstrap');
+
+    wp_register_style('bootstrap-rfs', get_template_directory_uri() . '/css/vendor/bootstrap-rfs.css', '', false, 'all');
+    wp_enqueue_style('bootstrap-rfs');
+
+    wp_register_style('normalize', get_template_directory_uri() . '/css/vendor/normalize.min.css', '', '8.0.1', 'all');
+    wp_enqueue_style('normalize');
+
+    wp_register_style('app', get_template_directory_uri() . '/css/app.css', '', '1.0.0', 'all');
+    wp_enqueue_style('app');
+}
+add_action('wp_enqueue_scripts', 'load_stylesheet');
+
+function load_jquery()
+{
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', get_template_directory_uri() . '/js/vendor/jquery-3.4.1.min.js', '', '3.4.1', true);
+}
+add_action('wp_enqueue_scripts', 'load_jquery');
+
+
+function load_javascript()
+{
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap.min.js', 'jquery', '4.4.1', true);
+    wp_enqueue_script('aos', get_template_directory_uri() . '/js/vendor/aos.js', 'jquery', false, true);
+    wp_enqueue_script('custom', get_template_directory_uri() . '/js/custom.js', 'jquery', false, true);
+}
+add_action('wp_enqueue_scripts', 'load_javascript');
+
+// Add menu feature in WordPress
+add_theme_support('menus');
+
+// Add thumbnail feature in Wordpress
+add_theme_support('post-thumbnails');
+
+// Add templates feature in Wordpress
+add_theme_support('templates');
+
+// Add Boostrap Menu file with Wordpress
+function register_navwalker()
+{
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action('after_setup_theme', 'register_navwalker');
+
+register_nav_menus(
+    array(
+        'menu' => __("Menu", 'theme')
+    )
+);
+
+function console($data)
+{
+    echo "<script>console.log('" . json_encode($data) . "');</script>";
+}
+
+// turn off wysiwig for custom post types
+add_action('init', 'init_remove_support', 100);
+function init_remove_support()
+{
+    remove_post_type_support('page', 'editor');
+}
+
+@ini_set('upload_max_size', '100M');
+@ini_set('post_max_size', '100M');
+@ini_set('max_execution_time', '300');
---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Silent Affirmations</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500&family=Manrope:wght@300;400;600&family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/images/favicon/apple-touch-icon.png'; ?>">
+    <link rel=" icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . '/images/favicon/favicon-32x32.png'; ?>">
+    <link rel=" icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . '/images/favicon/favicon-16x16.png'; ?>">
+    <link rel=" manifest" href="<?php echo get_template_directory_uri() . '/images/favicon/site.webmanifest'; ?>">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class('main'); ?>>
+
+    <header class=" sticky-top" role="banner">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
+            <div class="container">
+                <a class="navbar-brand mx-auto" href=<?php bloginfo('url'); ?>>
+                    <img src="<?php echo get_template_directory_uri() . '/images/logo.png' ?>" alt="" class="align-middle navbar-logo img-fluid" />
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <?php
+                wp_nav_menu(array(
+                    'theme_location'    => 'menu',
+                    'depth'             => 2,
+                    'container'         => 'div',
+                    'container_class'   => 'collapse navbar-collapse',
+                    'container_id'      => 'navbar',
+                    'menu_class'        => 'nav navbar-nav mx-auto text-center font-manrope font-weight-bold',
+                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'            => new WP_Bootstrap_Navwalker(),
+                ));
+                ?>
+            </div>
+        </nav>
+    </header>
+
+    <main role="main">
