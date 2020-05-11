045705da289dacc56fe40274a0240b9e2576b23f
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2020-05-11 17:22:08 -0600

    Footer

--- footer.php
+++ footer.php
@@ -1,5 +1,18 @@
 </main>
 
+<div class="bg-peter-river">
+
+    <footer class="container py-5">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">
+                    Dr. Sheel Tangri
+                </h1>
+            </div>
+        </div>
+    </footer>
+</div>
+
 <?php wp_footer(); ?>
 
 </body>
